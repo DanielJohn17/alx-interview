9d829d31b4168077453d07ad907ea200f9a91f74
DanielJohn17 <danielyohannes38@gmail.com>
2024-08-23 18:29:47 +0300

    finished task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Module for minOperations method.'''
+
+
+def minOperations(n: int) -> int:
+    '''
+        Method that calculates the fewest number of operations
+        needed to result in exactly n H characters in the file.
+    '''
+    file = "H"
+    temp = ""
+    operations = 0
+
+    if n <= 0:
+        return 0
+
+    while len(file) < n:
+        if n % len(file) == 0:
+            temp = file
+            operations += 2
+        else:
+            operations += 1
+
+        file += temp
+
+    return operations
