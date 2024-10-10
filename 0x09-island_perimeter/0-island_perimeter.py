5aef2d354961bff148ce0c8127be1f16f043cec9
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-10 21:02:08 +0300

    finished task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module for island_perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    Function that returns the perimeter of the island described in grid
+    """
+    n = len(grid)
+    m = len(grid[0])
+    perimeter = 0
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1:
+                if i == 0:
+                    perimeter += 1
+                elif grid[i-1][j] == 0:
+                    perimeter += 1
+                if i == n-1:
+                    perimeter += 1
+                elif grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == 0:
+                    perimeter += 1
+                elif grid[i][j-1] == 0:
+                    perimeter += 1
+                if j == m-1:
+                    perimeter += 1
+                elif grid[i][j+1] == 0:
+                    perimeter += 1
+    return perimeter
